18058b5821fe2d380a39516d1b0263a80ad15bd7
AlexP <alex.p.panteleev@gmail.com>
2020-07-21 11:47:20 +0300

    [i] add constants

--- components/rbp.copyrights/.description.php
+++ components/rbp.copyrights/.description.php
@@ -6,15 +6,17 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 
 use Bitrix\Main\Localization\Loc;
 
+require_once '../../constants.php';
+
 $arComponentDescription = [
     "NAME" => Loc::getMessage("A2C_RBP_COPYRIGHTS_COMPONENT_NAME"),
     "DESCRIPTION" => Loc::getMessage("A2C_RBP_COPYRIGHTS_DESCRIPTION"),
     "SORT" => 5,
     "CACHE_PATH" => "Y",
     "PATH" => [
-        "ID" => "A2C",
+        "ID" => PARTNER_NAME,
         "CHILD" => [
-            "ID" => "resume_block_pack",
+            "ID" => MODULE_NAME,
             "NAME" => Loc::getMessage("A2C_RBP_COPYRIGHTS_GROUP_NAME"),
             "SORT" => 30,
         ],
