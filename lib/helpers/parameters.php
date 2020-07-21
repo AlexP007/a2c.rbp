5f2a28905e5b55bc630dfbe0cb64c70d6bb4438e
AlexP <alex.p.panteleev@gmail.com>
2020-07-21 16:57:38 +0300

    [i] implement helpers/parameters

---
+++ lib/helpers/parameters.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace A2C\RBP\Helpers;
+
+
+/**
+ * Класс упрощающий работу с созданием
+ * файла .parameters.php
+ *
+ * Class Parameters
+ * @package A2C\RBP\Helpers
+ * @license MIT
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/a2c.checkout
+ */
+class Parameters
+{
+    /**
+     * Возвращает список инфоблоков
+     *
+     * @return array
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ObjectPropertyException
+     * @throws \Bitrix\Main\SystemException
+     */
+    public static function getIBlocks(): array
+    {
+        $iblockList = Iblock::getIbclocksIdName();
+        self::setPrompt($iblockList, 'Выберите инфоблок');
+        return array_column($iblockList, 'NAME', 'ID');
+    }
+
+    /**
+     * Возвращает список элементов инфоблока
+     *
+     * @param int $id
+     * @return array
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ObjectPropertyException
+     * @throws \Bitrix\Main\SystemException
+     */
+    public static function getIElements(int $id): array
+    {
+        $eltsList = Iblock::getIblockElements($id);
+        return array_column($eltsList, 'NAME', 'ID');
+    }
+
+    /**
+     * Возвращает свойства инфоблока
+     *
+     * @param int $id
+     * @return array
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ObjectPropertyException
+     * @throws \Bitrix\Main\SystemException
+     */
+    public static function getIProps(int $id): array
+    {
+        $propsList = Iblock::getPropertyIdName(['IBLOCK_ID' => $id]);
+        self::setPrompt($propsList, 'Выберите свойства');
+        return array_column($propsList, 'NAME', 'ID');
+    }
+
+    /**
+     * Устанавливает значение по умолчанию
+     * для выподающего списка
+     *
+     * @param array $list
+     * @param string $promptText
+     */
+    private static function setPrompt(array &$list ,string $promptText)
+    {
+        array_push($list, ['ID' => '0', 'NAME' => $promptText]);
+    }
+}
