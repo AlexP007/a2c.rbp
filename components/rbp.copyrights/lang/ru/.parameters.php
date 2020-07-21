0dc57ebe5b626e35899f40029d37267d6b6a6a96
AlexP <alex.p.panteleev@gmail.com>
2020-07-21 12:31:29 +0300

    [i] copyrights .parameters new comments

--- components/rbp.copyrights/lang/ru/.parameters.php
+++ components/rbp.copyrights/lang/ru/.parameters.php
@@ -1,7 +1,7 @@
 <?php
 // LOC
-$MESS['A2C_RBP_COPYRIGHTS_YEAR'] = 'Год созания сайта';
-$MESS['A2C_RBP_COPYRIGHTS_TEXT'] = 'Текстовый копирайт';
+$MESS['A2C_RBP_COPYRIGHTS_YEAR'] = 'Год созания сайта (число)';
+$MESS['A2C_RBP_COPYRIGHTS_TEXT'] = 'Текст копирайта';
 
 // TOOLTIP
 $MESS['YEAR_TIP'] = 'Если не задан, то будет установлен текущий';
