c91aa49e02f7b2217bd266af6d2840d986ac00e0
AlexP <alex.p.panteleev@gmail.com>
2020-07-23 22:18:28 +0300

    [i] add cropPicture method

--- lib/component/basic.php
+++ lib/component/basic.php
@@ -5,6 +5,7 @@ namespace A2C\RBP\Component;
 
 
 use CBitrixComponent;
+use CFile;
 
 
 /**
@@ -21,5 +22,24 @@ class Basic extends CBitrixComponent
     public function onPrepareComponentParams($arParams)
     {
         return parent::onPrepareComponentParams($arParams);
+    }
+
+    protected function cropPicture($id)
+    {
+        $width = $this->arParams['IMAGE_WIDTH'] > 0
+            ? $this->arParams['IMAGE_WIDTH'] : 100;
+
+        $height = $this->arParams['IMAGE_WIDTH'] > 0
+            ? $this->arParams['IMAGE_HEIGHT'] : 100;
+
+        return CFile::ResizeImageGet(
+            $id,
+            [
+                'width' => $width,
+                'height' => $height,
+            ],
+            BX_RESIZE_IMAGE_PROPORTIONAL,
+            true
+        );
     }
 }
