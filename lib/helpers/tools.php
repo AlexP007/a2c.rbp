cf474b0c1e1c33986ba4ae679b7d4286ab878ba3
AlexP <alex.p.panteleev@gmail.com>
2020-07-24 12:56:43 +0300

    [i] implement getDateDiff()

--- lib/helpers/tools.php
+++ lib/helpers/tools.php
@@ -4,6 +4,12 @@
 namespace A2C\RBP\Helpers;
 
 
+use DateTime;
+
+use Csite;
+use CDatabase;
+use DateInterval;
+
 /**
  * Свалка полезных методов
  *
@@ -23,11 +29,36 @@ class Tools
         return true;
     }
 
+    /**
+     * По номеру страны из настроек сайта
+     * вовзращает её название.
+     *
+     * @param int $i
+     * @return string
+     */
     public static function getUserCountry(int $i): string
     {
         $countries = GetCountryArray();
         $map = array_flip($countries['reference_id']);
         $countryId = $map[$i];
         return $countries['reference'][$countryId];
+    }
+
+    /**
+     * Принимает строку короткую времени в формате сайта.
+     * Возвращает обьект php DateInterval - разница между
+     * переданным временем и текущим.
+     *
+     * @param string $date
+     * @return DateInterval
+     * @throws \Exception
+     */
+    public static function getDateDiff(string $date): DateInterval
+    {
+        $siteFormat = CDatabase::DateFormatToPHP(CSite::GetDateFormat('SHORT'));
+        $siteDate = DateTime::createFromFormat($siteFormat, $date);
+        $nowDate = new DateTime();
+
+        return $siteDate->diff($nowDate);
     }
 }
