1f963f0a4fc1abe519701fd00b116c9a50532cea
AlexP <alex.p.panteleev@gmail.com>
2020-07-25 18:41:28 +0300

    [i] component animated_icon with description and template

---
+++ components/animated_icon/lang/ru/.description.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Компонент Анимированная иконка
+ * Языковые файлы
+ *
+ * @license MIT
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/a2c.rbp
+ */
+
+$MESS['A2C_RBP_ANIMATED_ICON_COMPONENT_NAME'] = 'Анимированная иконка';
+$MESS['A2C_RBP_ANIMATED_ICON_COMPONENT_DESCRIPTION'] = 'Анимированная иконка с эффектом движения вверх-вниз';
+$MESS['A2C_RBP_ANIMATED_ICON_MODULE_NAME'] = 'Блог и резюме';
+?>
