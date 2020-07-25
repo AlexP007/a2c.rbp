7e0722c344d0769375448c36a161fa24d807369d
AlexP <alex.p.panteleev@gmail.com>
2020-07-25 19:08:32 +0300

    [i] implement animated_icon class

---
+++ components/animated_icon/class.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die;
+}
+
+use Bitrix\Main\Loader;
+
+use A2C\RBP\Helpers\{Assets, Tools};
+
+Loader::includeModule('a2c.rbp') or Tools::showModuleError('a2c.rbp');
+
+/**
+ * Компонент Анимированная иконка
+ *
+ * Class A2cRbpAnimatedIcon
+ * Иконка с эффектом подпрыгивания
+ *
+ * @license MIT
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/a2c.rbp
+ */
+class A2cRbpAnimatedIcon extends Basic
+{
+    public function executeComponent()
+    {
+        Assets::initJs();
+        $this->includeComponentTemplate();
+    }
+}
