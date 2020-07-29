d3c38991f8a2c81ceafe289a08cf0e2c4a83f8df
AlexP <alex.p.panteleev@gmail.com>
2020-07-29 21:01:09 +0300

    [i] add doc-comments

--- components/infosite/.description.php
+++ components/infosite/.description.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Компонент Инфо-сайт
+ * Описание
+ *
+ * @license MIT
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/a2c.rbp
+ */
+
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die;
 }
--- components/infosite/lang/ru/.description.php
+++ components/infosite/lang/ru/.description.php
@@ -1,4 +1,17 @@
 <?php
+
+/**
+ * Компонент Инфо-сайт
+ * Языковые файлы
+ *
+ * @license MIT
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/a2c.rbp
+ */
+
+
 $MESS['A2C_RBP_INFOSITE_COMPONENT_NAME'] = 'Инфо-сайт';
 $MESS['A2C_RBP_INFOSITE_COMPONENT_DESCRIPTION'] = 'Комплексный компонент, реализующий вывод информации по инфоблокам -> разделам -> элементам';
 $MESS['A2C_RBP_INFOSITE_MODULE_NAME'] = 'Блог и резюме';
