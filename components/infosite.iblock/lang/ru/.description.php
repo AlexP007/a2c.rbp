54511f54c31c807cb75ab44696bd157308f69352
AlexP <alex.p.panteleev@gmail.com>
2020-08-01 12:48:23 +0300

    [i] comments

--- components/infosite.iblock/lang/ru/.description.php
+++ components/infosite.iblock/lang/ru/.description.php
@@ -1,4 +1,16 @@
 <?php
+
+/**
+ * Компонент Инфо-сайт инфоблок
+ * Языковые файлы
+ *
+ * @license MIT
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/a2c.rbp
+ */
+
 $MESS['A2C_RBP_INFOSITE_IBLOCK_COMPONENT_NAME'] = 'Список инфоблоков';
 $MESS['A2C_RBP_INFOSITE_IBLOCK_COMPONENT_DESCRIPTION'] = 'Компонент, реализующий вывод информации по инфоблокам определенного типа';
 $MESS['A2C_RBP_INFOSITE_IBLOCK_MODULE_NAME'] = 'Блог и резюме';
