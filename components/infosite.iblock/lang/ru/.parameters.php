578f4c89259f8265a4e48689087c686aec58da23
AlexP <alex.p.panteleev@gmail.com>
2020-08-02 13:42:18 +0300

    [i] add properties to infosite.iblock

---
+++ components/infosite.iblock/lang/ru/.parameters.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Компонент Инфо-сайт инфоблок
+ * Языковые файлы
+ *
+ * @license MIT
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/a2c.rbp
+ */
+
+// PARAMETERS
+$MESS['A2C_RBP_INFOSITE_IBLOCK_IBLOCK_TYPE'] = 'Тип инфоблока';
+?>
