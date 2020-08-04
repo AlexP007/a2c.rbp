ce9c778a108a79b0bb5da6f4ad5a88f2d4d58805
AlexP <alex.p.panteleev@gmail.com>
2020-08-04 21:44:37 +0300

    [f] codestyle

--- components/infosite.iblocks/templates/.default/template.php
+++ components/infosite.iblocks/templates/.default/template.php
@@ -26,4 +26,3 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     </li>
 <?php endforeach;?>
 </ul>
-
--- components/infosite.sections/templates/.default/template.php
+++ components/infosite.sections/templates/.default/template.php
@@ -27,5 +27,3 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     </li>
 <?php endforeach;?>
 </ul>
-
-
