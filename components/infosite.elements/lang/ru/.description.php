52722f3ba42bc4095c1fc222d185a5d71fb4ac91
AlexP <alex.p.panteleev@gmail.com>
2020-08-04 21:55:15 +0300

    [i] infosite.elements basic struct

---
+++ components/infosite.elements/lang/ru/.description.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Компонент Инфо-сайт Элементы секций
+ * Языковые файлы
+ *
+ * @license MIT
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/a2c.rbp
+ */
+
+$MESS['A2C_RBP_INFOSITE_ELEMENTS_COMPONENT_NAME'] = 'Элементы секций';
+$MESS['A2C_RBP_INFOSITE_ELEMENTS_COMPONENT_DESCRIPTION'] = 'Компонент, реализующий вывод элементов конкретной секции инфоблока';
+$MESS['A2C_RBP_INFOSITE_ELEMENTS_MODULE_NAME'] = 'Блог и резюме';
+?>
---
+++ components/infosite.elements/templates/.defaut/template.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die;
+}
+
+/**
+ * Компонент Инфо-сайт Элементы секций
+ * Языковые файлы
+ *
+ * @license MIT
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/a2c.rbp
+ */
