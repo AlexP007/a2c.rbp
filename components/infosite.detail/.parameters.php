533a51787f416a46c480b3fc344233787cae49d8
AlexP <alex.p.panteleev@gmail.com>
2020-08-08 19:22:35 +0300

    [i] infosite.detail .parameters

--- components/infosite.detail/.parameters.php
+++ components/infosite.detail/.parameters.php
@@ -14,3 +14,54 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
  * @email alex.p.panteleev@gmail.com
  * @link https://github.com/AlexP007/a2c.rbp
  */
+
+$arComponentParameters = [
+    "PARAMETERS" => [
+        "IBLOCK_FILTER_KEY" => [
+            "PARENT" => "DATA_SOURCE",
+            "NAME" => Loc::getMessage("A2C_RBP_INFOSITE_DETAIL_IBLOCK_FILTER_KEY"),
+            "TYPE" => "STRING",
+        ],
+        "IBLOCK_FILTER_VALUE" => [
+            "PARENT" => "DATA_SOURCE",
+            "NAME" => Loc::getMessage("A2C_RBP_INFOSITE_DETAIL_IBLOCK_FILTER_VALUE"),
+            "TYPE" => "STRING",
+        ],
+        "SECTION_FILTER_KEY" => [
+            "PARENT" => "DATA_SOURCE",
+            "NAME" => Loc::getMessage("A2C_RBP_INFOSITE_DETAIL_SECTION_FILTER_KEY"),
+            "TYPE" => "STRING",
+        ],
+        "SECTION_FILTER_VALUE" => [
+            "PARENT" => "DATA_SOURCE",
+            "NAME" => Loc::getMessage("A2C_RBP_INFOSITE_DETAIL_SECTION_FILTER_VALUE"),
+            "TYPE" => "STRING",
+        ],
+        "ELEMENT_FILTER_KEY" => [
+            "PARENT" => "DATA_SOURCE",
+            "NAME" => Loc::getMessage("A2C_RBP_INFOSITE_DETAIL_ELEMENT_FILTER_KEY"),
+            "TYPE" => "STRING",
+        ],
+        "ELEMENT_FILTER_VALUE" => [
+            "PARENT" => "DATA_SOURCE",
+            "NAME" => Loc::getMessage("A2C_RBP_INFOSITE_DETAIL_ELEMENT_FILTER_VALUE"),
+            "TYPE" => "STRING",
+        ],
+        "USE_ELEMENT_PROPERTIES" => [
+            "PARENT"  => "ADDITIONAL_SETTINGS",
+            "NAME"    => Loc::getMessage('A2C_RBP_INFOSITE_DETAIL_USE_ELEMENT_PROPERTIES'),
+            "TYPE"    => "CHECKBOX",
+            "DEFAULT" => "N"
+        ],
+        "IMAGE_HEIGHT" => [
+            "PARENT" => "ADDITIONAL_SETTINGS",
+            "NAME"   => Loc::getMessage("A2C_RBP_INFOSITE_DETAIL_IMAGE_HEIGHT"),
+            "TYPE"   => "STRING",
+        ],
+        "IMAGE_WIDTH"  => [
+            "PARENT" => "ADDITIONAL_SETTINGS",
+            "NAME"   => Loc::getMessage("A2C_RBP_INFOSITE_DETAIL_IMAGE_WIDTH"),
+            "TYPE"   => "STRING",
+        ],
+    ]
+];
--- components/infosite.detail/lang/ru/.parameters.php
+++ components/infosite.detail/lang/ru/.parameters.php
@@ -11,4 +11,23 @@
  * @link https://github.com/AlexP007/a2c.rbp
  */
 
+// PARAMETERS
+$MESS['A2C_RBP_INFOSITE_DETAIL_IBLOCK_FILTER_KEY'] = 'Ключ для фильтра инфоблока';
+$MESS['A2C_RBP_INFOSITE_DETAIL_IBLOCK_FILTER_VALUE'] = 'Значение ключа фильтра инфоблока';
+$MESS['A2C_RBP_INFOSITE_DETAIL_SECTION_FILTER_KEY'] = 'Ключ для фильтра секции';
+$MESS['A2C_RBP_INFOSITE_DETAIL_SECTION_FILTER_VALUE'] = 'Значение ключа фильтра секции';
+$MESS['A2C_RBP_INFOSITE_DETAIL_ELEMENT_FILTER_KEY'] = 'Ключ для фильтра элемента';
+$MESS['A2C_RBP_INFOSITE_DETAIL_ELEMENT_FILTER_VALUE'] = 'Значение ключа фильтра элеметна';
+$MESS['A2C_RBP_INFOSITE_DETAIL_USE_ELEMENT_PROPERTIES'] = 'Использовать свойства';
+$MESS['A2C_RBP_INFOSITE_DETAIL_IMAGE_HEIGHT'] = 'Высота фото (картинка для анонса)';
+$MESS['A2C_RBP_INFOSITE_DETAIL_IMAGE_WIDTH'] = 'Ширина фото (картинка для анонса)';
+
+// TOOLTIPS
+$MESS['IBLOCK_FILTER_KEY_TIP'] = 'Будет использован при выборке';
+$MESS['IBLOCK_FILTER_VALUE_TIP'] = 'Будет использовано при выборке';
+$MESS['SECTION_FILTER_KEY_TIP'] = 'Будет использован при выборке';
+$MESS['SECTION_FILTER_VALUE_TIP'] = 'Будет использовано при выборке';
+$MESS['ELEMENT_FILTER_KEY_TIP'] = 'Будет использован при выборке';
+$MESS['ELEMENT_FILTER_VALUE_TIP'] = 'Будет использовано при выборке';
+$MESS['USE_ELEMENT_PROPERTIES_TIP'] = 'При выборке, будет сделан запрос за всеми свойства';
 ?>
