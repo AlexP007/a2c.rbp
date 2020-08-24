c83789384099894fb8aa9555533d04bb51140a2a
AlexP <alex.p.panteleev@gmail.com>
2020-08-24 22:29:13 +0300

    [f]

--- components/accordion/templates/.default/template.php
+++ components/accordion/templates/.default/template.php
@@ -16,22 +16,22 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die;
 }
 ?>
-<h4 <?php if (isset($arParams['HEADING_CLASS'])): ?>
-        class="<?=$arParams['HEADING_CLASS']?>"
+<h4 <?php if (isset($arParams['~HEADING_CLASS'])): ?>
+        class="<?=$arParams['~HEADING_CLASS']?>"
     <?php endif; ?>
 >
     <a
         data-type="rbp-acc-opener"
-        data-down="<?=$arParams['ICON_DOWN']?>"
-        data-up="<?=$arParams['ICON_UP']?>"
-        href="#lab<?=$arParams['ID']?>"
+        data-down="<?=$arParams['~ICON_DOWN']?>"
+        data-up="<?=$arParams['~ICON_UP']?>"
+        href="#lab<?=$arParams['~ID']?>"
     >
-        <?=$arParams['HEADING']?> <i class="<?=$arParams['ICON_DOWN']?>"></i>
+        <?=$arParams['~HEADING']?> <i class="<?=$arParams['~ICON_DOWN']?>"></i>
     </a>
 </h4>
 <div class="rbp-display-none" id="lab<?=$arParams['ID']?>">
-    <?php if (isset($arParams['CAPTURE'])): ?>
-        <div><?=$arParams['CAPTURE']?></div>
+    <?php if (isset($arParams['~CAPTURE'])): ?>
+        <div><?=$arParams['~CAPTURE']?></div>
     <?php endif; ?>
-    <div><?=$arParams['MAIN']?></div>
+    <div><?=$arParams['~MAIN']?></div>
 </div>
